98ceb129e9c87d352fcb839a417fa1a6e5555b7b
aleshaujay <aleshaujay@icloud.com>
2024-07-27 04:34:59 +0700

    work with logic and more 🤷‍♂️

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    @IBOutlet var greetingLabel: UILabel!
+    @IBOutlet var greetingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        greetingLabel.isHidden.toggle()
+        greetingButton.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction func greetingButtonDidTapped(_ sender: UIButton) {
+        greetingLabel.isHidden.toggle()
+        sender.setTitle(
+            greetingLabel.isHidden ? "Show greeting" : "Hide greeting",
+            for: .normal
+        )
+        
+        /*
+         это код 👇 можно оптимизировать использовав 👆Тернарный оператор
+         if greetingLabel.isHidden {
+            sender.setTitle("Show greeting", for: .normal)
+        } else {
+            sender.setTitle("Hide greeting", for: .normal)
+        }
+         */
+    }
+    
+    
 }
 
